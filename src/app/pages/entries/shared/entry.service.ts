21c941517ee4c2527a91e86dd6588ca0105bb972
Thiago Ericson <ericson.thiago@gmail.com>
2019-01-27 00:55:30 -0200

    improving EntryService

--- src/app/pages/entries/shared/entry.service.ts
+++ src/app/pages/entries/shared/entry.service.ts
@@ -7,7 +7,7 @@ import { CategoryService } from "../../categories/shared/category.service";
 import { Entry } from "./entry.model";
 
 import { Observable } from "rxjs";
-import { flatMap } from "rxjs/operators";
+import { flatMap, catchError } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -22,32 +22,48 @@ export class EntryService extends BaseResourceService<Entry> {
      */
   }
 
-  create(entry: Entry): Observable<any> {
-    return this.categoryService.getById(entry.categoryId).pipe(
-      /**
-       * usa-se o flatMap() pois é um Observable dentro de outro Observable
-      */
-      flatMap(category => {
-        entry.category = category;
-        // return this.http.post(this.apiPath, entry).pipe(
-        //   catchError(this.handleError),
-        //   map(this.jsonDataToResource)
-        // )
-        return super.create(entry)
-      })
-    )
+  create(entry: Entry): Observable<Entry> {
+    // return this.categoryService.getById(entry.categoryId).pipe(
+    //   /**
+    //    * usa-se o flatMap() pois é um Observable dentro de outro Observable
+    //   */
+    //   flatMap(category => {
+    //     entry.category = category;
+    //     // return this.http.post(this.apiPath, entry).pipe(
+    //     //   catchError(this.handleError),
+    //     //   map(this.jsonDataToResource)
+    //     // )
+    //     return super.create(entry)
+    //   })
+    // )
+    // .bind(this) serve para forçar o contexto independente de onde a função for chamada
+    // no caso desta class, é chamado no flatMap, onde ele muda o contexto, por isso devemos forçar
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this))
   }
 
   update(entry: Entry): Observable<Entry> { // Depois dos dois pontos é o tipo do retorno
+    // return this.categoryService.getById(entry.categoryId).pipe(
+    //   flatMap(category => { // category é o retorno da requisição
+    //     entry.category = category
+    //     // return this.http.put(url, entry).pipe(
+    //     //   catchError(this.handleError),
+    //     //   map(() => entry) // força o retorno do mesmo objeto, pois no put não tem retorno do objeto
+    //     // )
+    //     return super.update(entry)
+    //   })
+    // )
+    // .bind(this) serve para forçar o contexto independente de onde a função for chamada
+    // no caso desta class, é chamado no flatMap, onde ele muda o contexto, por isso devemos forçar
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this))
+  }
+
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry> {
     return this.categoryService.getById(entry.categoryId).pipe(
-      flatMap(category => { // category é o retorno da requisição
-        entry.category = category
-        // return this.http.put(url, entry).pipe(
-        //   catchError(this.handleError),
-        //   map(() => entry) // força o retorno do mesmo objeto, pois no put não tem retorno do objeto
-        // )
-        return super.update(entry)
-      })
+      flatMap(category => {
+        entry.category = category;
+        return sendFn(entry)
+      }),
+      catchError(this.handleError)
     )
   }
 }
